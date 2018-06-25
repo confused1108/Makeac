3197f4f73c9d85137fbf27afd9f7a49fea7f3d75
Hitesh Ahuja <ahuja.hits812@yahoo.com>
2018-06-25 16:02:17 +0530

    Update db.php

--- APP CODE/server files/db.php
+++ APP CODE/server files/db.php
@@ -6,5 +6,5 @@
  * Time: 2:11 AM
  */
 header("Access-Control-Allow-Origin: *");
-$con = mysqli_connect("mysql.hostinger.in","u689854465_makac","7GkAX4wNhd1x","u689854465_makac") or die ("could not connect database");
+$con = mysqli_connect("","","","") or die ("could not connect database");
 ?>
